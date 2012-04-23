044df186295b54c72098421a07562e6060dcf32f
Jesse Donat <j.donat@capstonedigital.com>
2012-04-23 13:12:16 -0500

    Major overhaul with major improvements - support for Kindle Fire added

--- Source/UserAgentParser.php
+++ Source/UserAgentParser.php
@@ -6,35 +6,59 @@
 * @param string $u_agent
 * @return array an array with browser, version and platform keys
 */
-function UserAgentParser( $u_agent ) { 
+function UserAgentParser( &$u_agent ) { 
 
 	$data = array();
 
-	# ^.+?(?<platform>Android|iPhone|iPad|Windows|Macintosh|Windows Phone OS)(?: NT)*(?: [0-9.]+)*(;|\))
-	if (preg_match('/^.+?(?P<platform>Android|iPhone|iPad|Windows|Macintosh|Windows Phone OS)(?: NT)*(?: [0-9.]+)*(;|\))/im', $u_agent, $regs)) {
-		$data['platform'] = $regs['platform'];
-	} else {
-		$result = "";
+	if( preg_match('/\((.*?)\)/im', $u_agent, $regs) ) {
+
+		# (?<platform>Android|iPhone|iPad|Windows|Linux|Macintosh|Windows Phone OS|Silk)(?: NT)?(?:[ /][0-9._]+)*(;|$)
+		preg_match_all('%(?P<platform>Android|iPhone|iPad|Windows|Linux|Macintosh|Windows Phone OS|Silk)(?: NT)?(?:[ /][0-9._]+)*(;|$)%im', $regs[1], $result, PREG_PATTERN_ORDER);
+		$result['platform'] = array_unique($result['platform']);
+		if( count($result['platform']) > 1 ) {
+			if( ($key = array_search( 'Android', $result['platform'] )) !== false ) {
+				$data['platform']  = $result['platform'][$key];
+			}
+		}else{
+			$data['platform'] = $result['platform'][0];
+		}
+
 	}
 
 	# (?<browser>Camino|Kindle|Firefox|Safari|MSIE|AppleWebKit|Chrome|IEMobile|Opera)(?:[/ ])(?<version>[0-9.]+)
-	preg_match_all('%(?P<browser>Camino|Kindle|Firefox|Safari|MSIE|AppleWebKit|Chrome|IEMobile|Opera)(?:[/ ])(?P<version>[0-9.]+)%im', $u_agent, $result, PREG_PATTERN_ORDER);
+	preg_match_all('%(?P<browser>Camino|Kindle|Firefox|Safari|MSIE|AppleWebKit|Chrome|IEMobile|Opera|Silk|Lynx|Version)(?:[/ ])(?P<version>[0-9.]+)%im', $u_agent, $result, PREG_PATTERN_ORDER);
 
-	if( $result['browser'][0] == 'AppleWebKit' ) {
+	//print_r( $result );
+
+	if( ($key = array_search( 'Kindle', $result['browser'] )) !== false || ($key = array_search( 'Silk', $result['browser'] )) !== false ) {
+		$data['browser']  = $result['browser'][$key];
+		$data['platform'] = 'Kindle';
+		$data['version']  = $result['version'][$key];
+	}elseif( $result['browser'][0] == 'AppleWebKit' ) {
 		if( ( $data['platform'] == 'Android' && !($key = 0) ) || $key = array_search( 'Chrome', $result['browser'] ) ) {
 			$data['browser'] = 'Chrome';
+			if( ($vkey = array_search( 'Version', $result['browser'] )) !== false ) {
+				$key = $vkey;
+			}
 		}elseif( $key = array_search( 'Kindle', $result['browser'] ) ) {
 			$data['browser'] = 'Kindle';
 		}elseif( $key = array_search( 'Safari', $result['browser'] ) ) {
 			$data['browser'] = 'Safari';
+			if( ($vkey = array_search( 'Version', $result['browser'] )) !== false ) {
+				$key = $vkey;
+			}
 		}else{
 			$key = 0;
 			$data['browser'] = 'webkit';
 		}
+		
 		$data['version'] = $result['version'][$key];
-	}elseif( $key = array_search( 'Opera', $result['browser'] ) ) {
+	}elseif( ($key = array_search( 'Opera', $result['browser'] )) !== false ) {
 		$data['browser'] = $result['browser'][$key];
 		$data['version'] = $result['version'][$key];
+		if( ($key = array_search( 'Version', $result['browser'] )) !== false ) {
+			$data['version'] = $result['version'][$key];
+		}
 	}elseif( $result['browser'][0] == 'MSIE' ){
 		if( $key = array_search( 'IEMobile', $result['browser'] ) ) {
 			$data['browser'] = 'IEMobile';
@@ -43,13 +67,12 @@ function UserAgentParser( $u_agent ) {
 			$key = 0;
 		}
 		$data['version'] = $result['version'][$key];
+	}elseif( $key = array_search( 'Kindle', $result['browser'] ) ) {
+			$data['browser'] = 'Kindle';
+			$data['platform'] = 'Kindle';
 	}else{
 		$data['browser'] = $result['browser'][0];
 		$data['version'] = $result['version'][0];
-	}
-
-	if( $data['browser'] == 'Kindle' ) {
-		$data['platform'] = 'Kindle';
 	}
 
 	return $data;
