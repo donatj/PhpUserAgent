dfada45f41b1fc748822349b32dd723d8d3f3b0f
Jesse Donat <donatj@gmail.com>
2014-03-05 15:42:34 -0600

    Slight docs update

--- Source/UserAgentParser.php
+++ Source/UserAgentParser.php
@@ -6,7 +6,7 @@
  * @author Jesse G. Donat <donatj@gmail.com>
  * @link https://github.com/donatj/PhpUserAgent
  * @link http://donatstudios.com/PHP-Parser-HTTP_USER_AGENT
- * @param string|null $u_agent
+ * @param string|null $u_agent User agent string to parse or null. Uses $_SERVER['HTTP_USER_AGENT'] on NULL
  * @return array an array with browser, version and platform keys
  */
 function parse_user_agent( $u_agent = null ) {
