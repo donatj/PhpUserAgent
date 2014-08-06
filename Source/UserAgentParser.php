09dee992715a5bfc15c5aa2f396229cd899f135a
Jesse Donat <donatj@gmail.com>
2014-08-05 22:39:01 -0500

    InvalidArgumentException documentation

--- Source/UserAgentParser.php
+++ Source/UserAgentParser.php
@@ -7,13 +7,14 @@
  * @link https://github.com/donatj/PhpUserAgent
  * @link http://donatstudios.com/PHP-Parser-HTTP_USER_AGENT
  * @param string|null $u_agent User agent string to parse or null. Uses $_SERVER['HTTP_USER_AGENT'] on NULL
+ * @throws InvalidArgumentException on not having a proper user agent to parse.
  * @return array an array with browser, version and platform keys
  */
 function parse_user_agent( $u_agent = null ) {
 	if( is_null($u_agent) ) {
-		if(isset($_SERVER['HTTP_USER_AGENT'])) {
+		if( isset($_SERVER['HTTP_USER_AGENT']) ) {
 			$u_agent = $_SERVER['HTTP_USER_AGENT'];
-		}else{
+		} else {
 			throw new \InvalidArgumentException('parse_user_agent requires a user agent');
 		}
 	}
