d26c57cf49fea2ee39d801be113c696604e9c335
toolstack <greg@toolstack.com>
2014-10-07 13:18:00 -0400

    Update UserAgentParser.php
    Added support for parsing short User Agent Strings

--- Source/UserAgentParser.php
+++ Source/UserAgentParser.php
@@ -57,7 +57,24 @@ function parse_user_agent( $u_agent = null ) {
 			(?:(?:[:/ ])(?P<version>[0-9A-Z.]+)|/(?:[A-Z]*))%ix',
 		$u_agent, $result, PREG_PATTERN_ORDER);
 
-
+	// If nothing has been found, handle cases like: "WordPress/3.7.1; http://wordpress.com" or "Googlebot/2.1 http://www.google.com/bot.html" or "FeedValidator/1.3"
+        if( !isset($result['browser'][0]) || !isset($result['version'][0]) ) {
+			if( preg_match( "/.*\/.*[; ]?.*/", $u_agent ) ) { 
+				$split = explode( "/", $u_agent );
+				$result['browser'][0] = $split[0];
+				
+				unset( $split[0] );
+				
+				$split = preg_split( "/[; ]/", implode( "/", $split ), 2 );
+				$result['version'][0] = $split[0];
+				
+				// If we didn't actually split on anything, leave the platform blank.
+				if( array_key_exists( 1, $split ) ) {
+					$platform = trim( $split[1] );
+				}
+			}
+		}
+			
 	// If nothing matched, return null (to avoid undefined index errors)
 	if( !isset($result['browser'][0]) || !isset($result['version'][0]) ) {
 		return $empty;
