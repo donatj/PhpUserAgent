0924da96ac47d9272a0d2f08d574bcdc2ba44ce3
Jesse Donat <donatj@gmail.com>
2014-10-07 13:02:19 -0500

    Rewrite of pull request #19 to be much simpler.

--- Source/UserAgentParser.php
+++ Source/UserAgentParser.php
@@ -27,6 +27,10 @@ function parse_user_agent( $u_agent = null ) {
 
 	if( !$u_agent ) return $empty;
 
+	if( preg_match('%^(?P<browser>[A-Z0-9]+)/(?P<version>[0-9A-Z.]+)$%i', $u_agent, $result) ) {
+		return array( 'platform' => null, 'browser' => $result['browser'], 'version' => $result['version'] );
+	}
+
 	if( preg_match('/\((.*?)\)/im', $u_agent, $parent_matches) ) {
 
 		preg_match_all('/(?P<platform>BB\d+;|Android|CrOS|iPhone|iPad|Linux|Macintosh|Windows(\ Phone)?|Silk|linux-gnu|BlackBerry|PlayBook|Nintendo\ (WiiU?|3DS)|Xbox(\ One)?)
@@ -56,24 +60,6 @@ function parse_user_agent( $u_agent = null ) {
 			(?:\)?;?)
 			(?:(?:[:/ ])(?P<version>[0-9A-Z.]+)|/(?:[A-Z]*))%ix',
 		$u_agent, $result, PREG_PATTERN_ORDER);
-
-	// If nothing has been found, handle cases like: "WordPress/3.7.1; http://wordpress.com" or "Googlebot/2.1 http://www.google.com/bot.html" or "FeedValidator/1.3"
-	if( !isset($result['browser'][0]) || !isset($result['version'][0]) ) {
-		if( preg_match("/.*\/.*[; ]?.*/", $u_agent) ) {
-			$split                = explode("/", $u_agent);
-			$result['browser'][0] = $split[0];
-
-			unset($split[0]);
-
-			$split                = preg_split("/[; ]/", implode("/", $split), 2);
-			$result['version'][0] = $split[0];
-
-			// If we didn't actually split on anything, leave the platform blank.
-			if( array_key_exists(1, $split) ) {
-				$platform = trim($split[1]);
-			}
-		}
-	}
 
 	// If nothing matched, return null (to avoid undefined index errors)
 	if( !isset($result['browser'][0]) || !isset($result['version'][0]) ) {
