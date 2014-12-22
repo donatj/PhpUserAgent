64807e992cd3e6c889e0f2d4f63c383e0f3bef94
Jesse Donat <donatj@gmail.com>
2014-12-22 14:12:58 -0600

    Very slight doc improvment

--- Source/UserAgentParser.php
+++ Source/UserAgentParser.php
@@ -8,7 +8,7 @@
  * @link http://donatstudios.com/PHP-Parser-HTTP_USER_AGENT
  * @param string|null $u_agent User agent string to parse or null. Uses $_SERVER['HTTP_USER_AGENT'] on NULL
  * @throws InvalidArgumentException on not having a proper user agent to parse.
- * @return array an array with browser, version and platform keys
+ * @return string[] an array with browser, version and platform keys
  */
 function parse_user_agent( $u_agent = null ) {
 	if( is_null($u_agent) ) {
