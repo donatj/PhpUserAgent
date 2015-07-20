e7acb8ba8e136ab3216e081d753e46f1d77091ba
Greg Ross <greg@toolstack.com>
2015-07-20 09:54:05 -0400

    - Fix to pass unit tests, but the logic is still broken.

--- Source/UserAgentParser.php
+++ Source/UserAgentParser.php
@@ -127,7 +127,10 @@ function parse_user_agent( $u_agent = null ) {
 		if( $ekey !== false ) { $key = $ekey; }
 		$version = $result['version'][$key];
 	} elseif( $browser == 'AppleWebKit' ) {
-		if( strpos($platform, 'BB') === 0 ) {
+		if( $platform == 'Android' ) {
+			$key = 0;
+			$browser = 'Android Browser';
+		} elseif( strpos($platform, 'BB') === 0 ) {
 			$browser  = 'BlackBerry Browser';
 			$platform = 'BlackBerry';
 		} elseif( $platform == 'BlackBerry' || $platform == 'PlayBook' ) {
