34ebfe00991b9ce29ace39f064fbb515c321dfa3
Jesse Donat <donatj@gmail.com>
2017-03-01 16:16:00 -0600

    Adds support for Puffin browser

--- Source/UserAgentParser.php
+++ Source/UserAgentParser.php
@@ -53,7 +53,7 @@ function parse_user_agent( $u_agent = null ) {
 	}
 
 	preg_match_all('%(?P<browser>Camino|Kindle(\ Fire)?|Firefox|Iceweasel|IceCat|Safari|MSIE|Trident|AppleWebKit|
-				TizenBrowser|Chrome|Vivaldi|IEMobile|Opera|OPR|Silk|Midori|Edge|CriOS|UCBrowser|SamsungBrowser|
+				TizenBrowser|Chrome|Vivaldi|IEMobile|Opera|OPR|Silk|Midori|Edge|CriOS|UCBrowser|Puffin|SamsungBrowser|
 				Baiduspider|Googlebot|YandexBot|bingbot|Lynx|Version|Wget|curl|
 				Valve\ Steam\ Tenfoot|
 				NintendoBrowser|PLAYSTATION\ (\d|Vita)+)
@@ -120,6 +120,19 @@ function parse_user_agent( $u_agent = null ) {
 	} elseif( $find('Opera', $key, $browser) ) {
 		$find('Version', $key);
 		$version = $result['version'][$key];
+	} elseif( $find('Puffin', $key, $browser) ) {
+		$version = $result['version'][$key];
+		if( strlen($version) > 3 ) {
+			$part = substr($version, -2);
+			if( ctype_upper($part) ) {
+				$version = substr($version, 0, -2);
+
+				$flags = array( 'IP' => 'iPhone', 'IT' => 'iPad', 'AP' => 'Android', 'AT' => 'Android', 'WP' => 'Windows Phone', 'WT' => 'Windows' );
+				if( isset($flags[$part]) ) {
+					$platform = $flags[$part];
+				}
+			}
+		}
 	} elseif( $find(array( 'IEMobile', 'Edge', 'Midori', 'Vivaldi', 'SamsungBrowser', 'Valve Steam Tenfoot', 'Chrome' ), $key, $browser) ) {
 		$version = $result['version'][$key];
 	} elseif( $rv_result && $find('Trident', $key) ) {
