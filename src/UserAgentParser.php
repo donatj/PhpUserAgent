f7c5c30c66fd6df485b6eca59163d72808808925
Jesse Donat <donatj@gmail.com>
2019-08-26 16:13:49 -0500

    https
    # Conflicts:
    #	src/UserAgentParser.php

--- src/UserAgentParser.php
+++ src/UserAgentParser.php
@@ -5,7 +5,7 @@
  *
  * @author Jesse G. Donat <donatj@gmail.com>
  * @link https://github.com/donatj/PhpUserAgent
- * @link http://donatstudios.com/PHP-Parser-HTTP_USER_AGENT
+ * @link https://donatstudios.com/PHP-Parser-HTTP_USER_AGENT
  * @param string|null $u_agent User agent string to parse or null. Uses $_SERVER['HTTP_USER_AGENT'] on NULL
  * @throws \InvalidArgumentException on not having a proper user agent to parse.
  * @return string[] an array with browser, version and platform keys
